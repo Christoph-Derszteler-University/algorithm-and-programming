47dd6ec8f84f5e98d69f875f0334fbaaaa4848e9
qetzing <general@qetz.de>
2024-10-22 20:16:46 +0200

    Improve sqrt.cpp code quality

--- assignments/third/sqrt.cpp
+++ assignments/third/sqrt.cpp
@@ -1,8 +1,27 @@
 #include <stdexcept>
 #include <cmath>
 #include <iostream>
+#include <format>
 
-double sqrt(double x, double tolerance) {
+constexpr double sqrt(double x, double tolerance);
+constexpr double sqrt(double x);
+
+int main() {
+  // Caution: Seems not to be working on windows
+  try {
+    sqrt(-1);
+  } catch (const std::domain_error& negative_x) {
+    std::cerr << negative_x.what() << std::endl;
+  }
+
+  std::cout << std::format("sqrt(4): {}", sqrt(4)) << std::endl;
+  std::cout << std::format("sqrt(2): {}", sqrt(2)) << std::endl;
+
+  std::cout << std::format("sqrt(4, 2.2): {}", sqrt(4, 2.2)) << std::endl;
+  std::cout << std::format("sqrt(2, 1.8): {}", sqrt(2, 1.8)) << std::endl;
+}
+
+constexpr double sqrt(double x, double tolerance) {
   if (x < 0) {
     throw std::domain_error("x must be greater/equal than zero!");
   }
@@ -16,21 +35,6 @@ double sqrt(double x, double tolerance) {
   return approximation;
 }
 
-double sqrt(double x) {
+constexpr double sqrt(double x) {
   return sqrt(x, 1e-8);
-}
-
-int main() {
-  // Caution: Seems not to be working on windows
-  try {
-    sqrt(-1);
-  } catch (const std::domain_error& negative_x) {
-    std::cerr << "Test 1: " << negative_x.what() << std::endl;
-  }
-
-  std::cout << "sqrt(4): " << sqrt(4) << std::endl;
-  std::cout << "sqrt(2): " << sqrt(2) << std::endl << std::endl;
-
-  std::cout << "sqrt(4, 2.2): " << sqrt(4, 2.2) << std::endl;
-  std::cout << "sqrt(2, 1.8): " << sqrt(2, 1.8) << std::endl << std::endl;
 }
